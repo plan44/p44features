plan44/p44features

commit a6c739b7cb87ecbf40ca24ebac53b86ba00a641d

Use improved p44utils logging now (with feature-level option to increase or decrease verbosity)

feature.cpp
@@ -49,13 +49,23 @@ ErrorPtr Feature::processRequest(ApiRequestPtr aRequest)
     }
     return FeatureApiError::err("Feature '%s': unknown cmd '%s'", getName().c_str(), cmd.c_str());
   }
+  else {
+    // decode properties
+    if (reqData->get("logleveloffset", o, true)) {
+      setLogLevelOffset(o->int32Value());
+      return ErrorPtr();
+    }
+  }
   return FeatureApiError::err("Feature '%s': cannot understand request (no cmd)", getName().c_str());
 }
 
 
 JsonObjectPtr Feature::status()
 {
-  if (isInitialized()) return JsonObject::newObj();
+  if (isInitialized()) {
+    JsonObjectPtr status = JsonObject::newObj();
+    status->add("logleveloffset", JsonObject::newInt32(getLogLevelOffset()));
+  }
   return JsonObject::newBool(false);
 }
 

dispmatrix.cpp
@@ -60,7 +60,7 @@ DispMatrix::DispMatrix(LEDChainArrangementPtr aLedChainArrangement) :
         initialize(cfg);
       }
       else {
-        LOG(LOG_ERR, "dispmatrix configuration failed: %s", err->text());
+        OLOG(LOG_ERR, "configuration failed: %s", err->text());
       }
     }
   }
@@ -87,7 +87,6 @@ DispMatrix::~DispMatrix()
 
 ErrorPtr DispMatrix::initialize(JsonObjectPtr aInitData)
 {
-  LOG(LOG_NOTICE, "initializing " FEATURE_NAME);
   reset();
   JsonObjectPtr o;
   ErrorPtr err;
@@ -324,7 +323,7 @@ void DispMatrix::initOperation()
     ledChainArrangement->begin(true);
   }
   else {
-    LOG(LOG_WARNING, FEATURE_NAME ": NOP: no ledchain connected");
+    OLOG(LOG_WARNING, "NOP: no ledchain connected");
   }
   setInitialized();
 }

feature.hpp
@@ -26,7 +26,7 @@
 
 namespace p44 {
 
-  class Feature : public P44Obj
+  class Feature : public P44LoggingObj
   {
 
     bool initialized;
@@ -35,6 +35,9 @@ namespace p44 {
   public:
 
     Feature(const string aName);
+
+    /// @return the prefix to be used for logging from this object
+    virtual string logContextPrefix() { return string_format("Feature '%s'", name.c_str()); }
 
     /// initialize the feature
     /// @param aInitData the init data object specifying feature init details
@@ -68,7 +71,7 @@ namespace p44 {
     void setInitialized() { initialized = true; }
 
     /// send event message
-    /// @param aEventMessage the API request to process
+    /// @param aMessage the API request to process
     /// @note event messages are messages sent by a feature without a preceeding request
     void sendEventMessage(JsonObjectPtr aMessage);
 

featureapi.cpp
@@ -51,7 +51,7 @@ void FeatureApiRequest::sendResponse(JsonObjectPtr aResponse, ErrorPtr aError)
     aResponse->add("Error", JsonObject::newString(aError->description()));
   }
   if (connection) connection->sendMessage(aResponse);
-  LOG(LOG_INFO,"API answer: %s", aResponse->c_strValue());
+  SOLOG(*FeatureApi::sharedApi(), LOG_INFO,"answer: %s", aResponse->c_strValue());
 }
 
 
@@ -61,7 +61,7 @@ void FeatureApiRequest::sendResponse(JsonObjectPtr aResponse, ErrorPtr aError)
 InternalRequest::InternalRequest(JsonObjectPtr aRequest) :
   inherited(aRequest)
 {
-  LOG(LOG_DEBUG,"Internal API request: %s", aRequest->c_strValue());
+  SOLOG(*FeatureApi::sharedApi(), LOG_DEBUG,"Internal request: %s", aRequest->c_strValue());
 }
 
 
@@ -72,7 +72,7 @@ InternalRequest::~InternalRequest()
 
 void InternalRequest::sendResponse(JsonObjectPtr aResponse, ErrorPtr aError)
 {
-  LOG(LOG_DEBUG,"Internal API answer: %s", aResponse->c_strValue());
+  SOLOG(*FeatureApi::sharedApi(), LOG_DEBUG,"Internal answer: %s", aResponse->c_strValue());
 }
 
 
@@ -134,7 +134,7 @@ ErrorPtr FeatureApi::runJsonFile(const string aScriptPath, SimpleCB aFinishedCal
   err = string_fromfile(fpath, jsonText);
   if (Error::notOK(err)) {
     err->prefixMessage("cannot open JSON file '%s': ", fpath.c_str());
-    LOG(LOG_WARNING, "Script loading error: %s", Error::text(err));
+    OLOG(LOG_WARNING, "Script loading error: %s", Error::text(err));
   }
   else {
     err = runJsonString(jsonText, aFinishedCallback, aContextP, aSubstitutionsP);
@@ -180,7 +180,7 @@ ErrorPtr FeatureApi::runJsonString(string aJsonString, SimpleCB aFinishedCallbac
       err = featureApi->executeJson(script, aFinishedCallback, aContextP);
     }
   }
-  if (!Error::isOK(err)) { LOG(LOG_WARNING, "Script execution error: %s", Error::text(err)); }
+  if (!Error::isOK(err)) { OLOG(LOG_WARNING, "Script execution error: %s", Error::text(err)); }
   return err;
 }
 
@@ -239,7 +239,7 @@ void FeatureApi::runCmd(JsonObjectPtr aCmds, int aIndex, FeatureJsonScriptContex
   }
   ApiRequestPtr req = ApiRequestPtr(new InternalRequest(cmd));
   ErrorPtr err = processRequest(req);
-  if (!Error::isOK(err)) { LOG(LOG_WARNING, "API script step execution error: %s", Error::text(err)); }
+  if (!Error::isOK(err)) { OLOG(LOG_WARNING, "script step execution error: %s", Error::text(err)); }
   executeNextCmd(aCmds, aIndex+1, aContext, aFinishedCallback);
 }
 
@@ -319,7 +319,7 @@ SocketCommPtr FeatureApi::apiConnectionHandler(SocketCommPtr aServerSocketComm)
 void FeatureApi::apiRequestHandler(JsonCommPtr aConnection, ErrorPtr aError, JsonObjectPtr aRequest)
 {
   if (Error::isOK(aError)) {
-    LOG(LOG_INFO,"API request: %s", aRequest->c_strValue());
+    OLOG(LOG_INFO,"request: %s", aRequest->c_strValue());
     ApiRequestPtr req = ApiRequestPtr(new FeatureApiRequest(aRequest, aConnection));
     aError = processRequest(req);
   }
@@ -328,7 +328,7 @@ void FeatureApi::apiRequestHandler(JsonCommPtr aConnection, ErrorPtr aError, Jso
     JsonObjectPtr resp = JsonObject::newObj();
     resp->add("Error", JsonObject::newString(aError->description()));
     aConnection->sendMessage(resp);
-    LOG(LOG_INFO,"API answer: %s", resp->c_strValue());
+    OLOG(LOG_INFO,"answer: %s", resp->c_strValue());
   }
 }
 
@@ -385,7 +385,7 @@ ErrorPtr FeatureApi::processRequest(ApiRequestPtr aRequest)
     }
     if (reqData->get("run", o, true)) {
       // directly run a script
-      queueScript(o->stringValue());
+      queueScript("API run cmd", o->stringValue());
       return Error::ok();
     }
     #endif // EXPRESSION_SCRIPT_SUPPORT
@@ -441,7 +441,7 @@ ErrorPtr FeatureApi::reset(ApiRequestPtr aRequest)
   for (FeatureMap::iterator f = featureMap.begin(); f!=featureMap.end(); ++f) {
     if (aRequest->getRequest()->get(f->first.c_str())) {
       featureFound = true;
-      LOG(LOG_NOTICE, "resetting feature '%s'", f->first.c_str());
+      OLOG(LOG_NOTICE, "resetting feature '%s'", f->first.c_str());
       f->second->reset();
     }
   }
@@ -466,7 +466,9 @@ ErrorPtr FeatureApi::init(ApiRequestPtr aRequest)
     JsonObjectPtr initData = reqData->get(f->first.c_str());
     if (initData) {
       featureFound = true;
+      SOLOG(*(f->second), LOG_NOTICE, "initializing...");
       err = f->second->initialize(initData);
+      SOLOG(*(f->second), LOG_NOTICE, "initialized: err=%s", Error::text(err));
       if (!Error::isOK(err)) {
         err->prefixMessage("Feature '%s' init failed: ", f->first.c_str());
         return err;
@@ -526,7 +528,7 @@ void FeatureApi::start(const string aApiPort)
   apiServer->setConnectionParams(NULL, aApiPort.c_str(), SOCK_STREAM, AF_INET6);
   apiServer->setAllowNonlocalConnections(true);
   apiServer->startServer(boost::bind(&FeatureApi::apiConnectionHandler, this, _1), 10);
-  LOG(LOG_INFO, "FeatureApi listening on port %s", aApiPort.c_str());
+  OLOG(LOG_INFO, "listening on port %s", aApiPort.c_str());
 }
 
 
@@ -535,19 +537,19 @@ void FeatureApi::sendMessage(JsonObjectPtr aMessage)
   #if EXPRESSION_SCRIPT_SUPPORT
   if (!eventScript.empty()) {
     // call event script
-    queueScript(eventScript, aMessage);
+    queueScript("API event", eventScript, aMessage);
   }
   #endif // EXPRESSION_SCRIPT_SUPPORT
   if (!connection) {
-    LOG(LOG_WARNING, "no API connection, message cannot be sent: %s", aMessage ? aMessage->json_c_str() : "<none>");
+    OLOG(LOG_WARNING, "no connection, message cannot be sent: %s", aMessage ? aMessage->json_c_str() : "<none>");
     return;
   }
   ErrorPtr err = connection->sendMessage(aMessage);
   if (Error::notOK(err)) {
-    LOG(LOG_ERR, "Error sending message to API: %s", err->text());
+    OLOG(LOG_ERR, "Error sending message: %s", err->text());
   }
   else {
-    LOG(LOG_INFO,"API event message: %s", aMessage->c_strValue());
+    OLOG(LOG_INFO,"event message: %s", aMessage->c_strValue());
   }
 }
 
@@ -617,7 +619,7 @@ bool FeatureApi::evaluateAsyncFeatureFunctions(EvaluationContext* aEvalContext, 
 
 void FeatureApi::apiCallFunctionDone(EvaluationContext* aEvalContext, JsonObjectPtr aResult, ErrorPtr aError)
 {
-  LOG(aEvalContext->getEvalLogLevel(), "feature api call returns '%s', error = %s", aResult->c_strValue(), Error::text(aError));
+  SOLOG(*FeatureApi::sharedApi(), LOG_INFO, "call returns '%s', error = %s", aResult->c_strValue(), Error::text(aError));
   ExpressionValue res;
   if (Error::isOK(aError)) {
     res.setJson(aResult);
@@ -629,10 +631,11 @@ void FeatureApi::apiCallFunctionDone(EvaluationContext* aEvalContext, JsonObject
 }
 
 
-void FeatureApi::queueScript(const string &aScriptCode, JsonObjectPtr aMessage)
+void FeatureApi::queueScript(const char *aScriptContextInfo, const string &aScriptCode, JsonObjectPtr aMessage)
 {
   FeatureApiScriptContextPtr script = FeatureApiScriptContextPtr(new FeatureApiScriptContext(aMessage));
   script->setCode(aScriptCode);
+  script->setContextInfo(aScriptContextInfo, this);
   scriptRequests.push_back(script);
   if (scriptRequests.size()==1) {
     // there was no script pending, must start
@@ -644,7 +647,7 @@ void FeatureApi::runNextScript()
 {
   if (!scriptRequests.empty()) {
     FeatureApiScriptContextPtr script = scriptRequests.front();
-    LOG(LOG_INFO, "+++ Starting feature API script");
+    OLOG(LOG_INFO, "+++ Starting script");
     script->execute(true, boost::bind(&FeatureApi::scriptDone, this, script));
   }
 }
@@ -652,7 +655,7 @@ void FeatureApi::runNextScript()
 
 void FeatureApi::scriptDone(FeatureApiScriptContextPtr aScript)
 {
-  LOG(LOG_INFO, "--- Finished feature API script");
+  OLOG(LOG_INFO, "--- Finished script");
   scriptRequests.pop_front();
   runNextScript();
 }

featureapi.hpp
@@ -165,7 +165,7 @@ namespace p44 {
 
 
 
-  class FeatureApi : public P44Obj
+  class FeatureApi : public P44LoggingObj
   {
     friend class FeatureApiRequest;
 
@@ -193,6 +193,10 @@ namespace p44 {
 
     FeatureApi();
     virtual ~FeatureApi();
+
+    /// @return the prefix to be used for logging from this object
+    virtual string logContextPrefix() { return "FeatureApi"; }
+
 
     /// singleton
     static FeatureApiPtr sharedApi();
@@ -260,7 +264,7 @@ namespace p44 {
     /// queue script for execution
     /// @param aScriptCode the code for the script
     /// @param aMessage the message related to the script call, which will be available from messsage() built-in function
-    void queueScript(const string &aScriptCode, JsonObjectPtr aMessage = NULL);
+    void queueScript(const char *aScriptContextInfo, const string &aScriptCode, JsonObjectPtr aMessage = NULL);
 
     void runNextScript();
     void scriptDone(FeatureApiScriptContextPtr aScript);

hermel.cpp
@@ -89,7 +89,6 @@ ErrorPtr HermelShoot::shoot(ApiRequestPtr aRequest)
 
 void HermelShoot::initOperation()
 {
-  LOG(LOG_NOTICE, "initializing hermel");
   setInitialized();
   endPulse();
 }

indicators.cpp
@@ -57,7 +57,6 @@ Indicators::~Indicators()
 
 ErrorPtr Indicators::initialize(JsonObjectPtr aInitData)
 {
-  LOG(LOG_NOTICE, "initializing " FEATURE_NAME);
   reset();
   // { "cmd":"init", "indicators": {} }
   // { "cmd":"init", "indicators": { "rootview": <p44lrgraphics-view-config> } }
@@ -218,7 +217,7 @@ void Indicators::stop()
 
 void Indicators::runEffect(P44ViewPtr aView, MLMicroSeconds aDuration, JsonObjectPtr aConfig)
 {
-  LOG(LOG_INFO, "Starting effect");
+  OLOG(LOG_INFO, "Starting effect");
   indicatorsView->pushView(aView);
   IndicatorEffectPtr effect = IndicatorEffectPtr(new IndicatorEffect);
   effect->view = aView;
@@ -235,7 +234,7 @@ void Indicators::effectDone(IndicatorEffectPtr aEffect)
   indicatorsView->removeView(aEffect->view);
   activeIndicators.remove(aEffect);
   indicatorsView->requestUpdate();
-  LOG(LOG_INFO, "Effect Done");
+  OLOG(LOG_INFO, "Effect Done");
 }
 
 
@@ -246,7 +245,7 @@ void Indicators::initOperation()
     ledChainArrangement->begin(true);
   }
   else {
-    LOG(LOG_WARNING, FEATURE_NAME ": NOP: no ledchain connected");
+    OLOG(LOG_WARNING, "NOP: no ledchain connected");
   }
   setInitialized();
 }

inputs.cpp
@@ -110,7 +110,6 @@ JsonObjectPtr Inputs::status()
 
 void Inputs::initOperation()
 {
-  LOG(LOG_NOTICE, "initializing " FEATURE_NAME);
   setInitialized();
 }
 

commit 73fb0e58c2cfdc30a07b72141dd135732c0b9748

feature status: fix regression (status always false) introduced with a6c739b - Use improved p44utils logging...

feature.cpp
@@ -65,7 +65,9 @@ JsonObjectPtr Feature::status()
   if (isInitialized()) {
     JsonObjectPtr status = JsonObject::newObj();
     status->add("logleveloffset", JsonObject::newInt32(getLogLevelOffset()));
+    return status;
   }
+  // not initialized
   return JsonObject::newBool(false);
 }
 